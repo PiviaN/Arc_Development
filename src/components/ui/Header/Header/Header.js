c02409b3683a44ad8bb5243bdb832615188b4b82
PiviaN <pedro.vsnep2001@gmail.com>
2021-05-10 21:29:01 -0300

    Implementing better active tab approach

--- src/components/ui/Header/Header/Header.js
+++ src/components/ui/Header/Header/Header.js
@@ -16,7 +16,7 @@ import {
   List,
   ListItem,
   ListItemText,
-  useTheme
+  useTheme,
 } from "@material-ui/core";
 
 import MenuIcon from "@material-ui/icons/Menu";
@@ -105,28 +105,25 @@ export default function Header(props) {
     { name: "Contact Us", link: "/contact", activeIndex: 4 },
   ];
 
+  const path = typeof window !== "undefined" ? window.location.pathname : null;
+
+  const activeIndex = () => {
+    const found = routes.find(({ link }) => link === path);
+    const menuFound = menuOptions.find(({ link }) => link === path);
+
+    if (menuFound) {
+      props.setValue(1);
+      props.setSelectedIndex(menuFound.selectedIndex);
+    } else if (found === undefined) {
+      props.setValue(false);
+    } else {
+      props.setValue(found.activeIndex);
+    }
+  };
+
   useEffect(() => {
-    [...menuOptions, ...routes].forEach((route) => {
-      switch (window.location.pathname) {
-        case `${route.link}`:
-          if (props.value !== route.activeIndex) {
-            props.setValue(route.activeIndex);
-            if (
-              route.selectedIndex &&
-              route.selectedIndex !== props.selectedIndex
-            ) {
-              props.setSelectedIndex(route.selectedIndex);
-            }
-          }
-          break;
-        case "/estimate":
-          props.setValue(5);
-          break;
-        default:
-          break;
-      }
-    });
-  }, [props.value, menuOptions, props.selectedIndex, routes, props]);
+    activeIndex();
+  }, [path]);
 
   const tabs = (
     <React.Fragment>
